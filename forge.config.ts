724a2796a075ab46003fcec3753b3829330a862e
Bin-Huang <tohuangbin@gmail.com>
2023-03-06 20:21:39 +0800

    init

---
+++ forge.config.ts
@@ -0,0 +1,35 @@
+import type { ForgeConfig } from '@electron-forge/shared-types';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
+import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+
+import { mainConfig } from './webpack.main.config';
+import { rendererConfig } from './webpack.renderer.config';
+
+const config: ForgeConfig = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  plugins: [
+    new WebpackPlugin({
+      mainConfig,
+      renderer: {
+        config: rendererConfig,
+        entryPoints: [
+          {
+            html: './src/index.html',
+            js: './src/renderer.ts',
+            name: 'main_window',
+            preload: {
+              js: './src/preload.ts',
+            },
+          },
+        ],
+      },
+    }),
+  ],
+};
+
+export default config;
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+}
+
+render();
