97ff3a9da4a73dc6def43033e8c68e609c9e1656
Bin-Huang <tohuangbin@gmail.com>
2023-03-07 14:23:58 +0800

    feat: 布局调整

--- src/devtools/App.tsx
+++ src/devtools/App.tsx
@@ -14,6 +14,7 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ChatIcon from '@mui/icons-material/Chat';
 import useStore from './store'
 import SettingWindow from './SettingWindow'
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 
 const { useEffect, useState } = React
 
@@ -30,154 +31,173 @@ function App() {
     const [openSettingWindow, setOpenSettingWindow] = React.useState(false);
 
     return (
-        <Grid container spacing={2} sx={{ background: "#FFFFFF" }}>
-            <Grid item xs={4}>
-                <Stack
+        <Box sx={{
+            height: '100%',
+            width: '100%',
+        }}>
+            <Grid container spacing={2} sx={{
+                background: "#FFFFFF",
+                height: '100%',
+            }}>
+                <Grid item xs={3}
                     sx={{
-                        bgcolor: '#F7F7F7',
-                        height: '90vh',
+                        height: '100%',
                     }}
-                    spacing={2}
                 >
-                    <Toolbar variant="dense">
-                        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                            <ChatIcon />
-                        </IconButton>
-                        <Typography variant="h5" color="inherit" component="div">
-                            ChatBox
-                        </Typography>
-                    </Toolbar>
-
-                    <Divider />
-
-                    <MenuList
+                    <Stack
                         sx={{
-                            width: '100%',
-                            // bgcolor: 'background.paper',
                             bgcolor: '#F7F7F7',
-                            position: 'relative',
-                            overflow: 'auto',
-                            height: '30vh',
-                            '& ul': { padding: 0 },
+                            height: '100%',
+                            padding: '20px 0',
                         }}
-                        component="nav"
-                        aria-labelledby="nested-list-subheader"
-                        subheader={
-                            <ListSubheader component="div"
-                                sx={{
-                                    bgcolor: '#F7F7F7',
-                                }}
-                            >
-                                Chat
-                            </ListSubheader>
-                        }
-
+                        spacing={2}
                     >
-                        {
-                            store.chatSessions.map((session, ix) => (
-                                <SessionItem selected={store.currentSession.id === session.id}
-                                    session={session}
-                                    switchMe={() => store.switchCurrentSession(session)}
-                                    deleteMe={() => store.deleteChatSession(session)}
-                                    copyMe={() => {
-                                        const newSession = createSession(session.name + ' Copyed')
-                                        newSession.messages = session.messages
-                                        store.createChatSession(newSession, ix)
-                                    }}
-                                    updateMe={(updated) => store.updateChatSession(updated)}
-                                />
-                            ))
-                        }
-                    </MenuList>
+                        <Toolbar variant="dense">
+                            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                                <ChatIcon />
+                            </IconButton>
+                            <Typography variant="h5" color="inherit" component="div">
+                                ChatBox
+                            </Typography>
+                        </Toolbar>
 
-                    <MenuList
-                        sx={{
-                            width: '100%',
-                            // bgcolor: 'background.paper',
-                            bgcolor: '#F7F7F7',
-                            position: 'relative',
-                            overflow: 'auto',
-                            height: '30vh',
-                            '& ul': { padding: 0 },
-                        }}
-                        component="nav"
-                        aria-labelledby="nested-list-subheader"
-                        subheader={
-                            <ListSubheader component="div"
-                                sx={{
-                                    bgcolor: '#F7F7F7',
-                                }}
-                            >
-                                Chat
-                            </ListSubheader>
-                        }
-
-                    >
-                        {
-                            store.chatSessions.map((session, ix) => (
-                                <SessionItem selected={store.currentSession.id === session.id}
-                                    session={session}
-                                    switchMe={() => store.switchCurrentSession(session)}
-                                    deleteMe={() => store.deleteChatSession(session)}
-                                    copyMe={() => {
-                                        const newSession = createSession(session.name + ' Copyed')
-                                        newSession.messages = session.messages
-                                        store.createChatSession(newSession, ix)
-                                    }}
-                                    updateMe={(updated) => store.updateChatSession(updated)}
-                                />
-                            ))
-                        }
-                    </MenuList>
-
-                    <Divider />
-
-                    <MenuItem onClick={() => store.createEmptyChatSession()} >
-                        <ListItemIcon>
-                            <IconButton><AddCircleOutlineIcon fontSize="small" /></IconButton>
-                        </ListItemIcon>
-                        <ListItemText>
-                            New Session
-                        </ListItemText>
-                        <Typography variant="body2" color="text.secondary">
-                            ⌘N
-                        </Typography>
-                    </MenuItem>
-                    <MenuItem onClick={() => {
-                        setOpenSettingWindow(true)
-                    }}
-                    >
-                        <ListItemIcon>
-                            <IconButton><AddCircleOutlineIcon fontSize="small" /></IconButton>
-                        </ListItemIcon>
-                        <ListItemText>
-                            Settings
-                        </ListItemText>
-                        <Typography variant="body2" color="text.secondary">
-                            ⌘N
-                        </Typography>
-                    </MenuItem>
-
-                </Stack>
-
-            </Grid>
-            <Grid item xs={8}>
-                <Box sx={{ height: '90vh', bgcolor: '#F7F7F7' }}>
-                    <Toolbar variant="dense" >
-                        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                            <ChatIcon />
-                        </IconButton>
-                        <Typography variant="h6" color="inherit" component="div" noWrap>
-                            {store.currentSession.name}
-                        </Typography>
                         <Divider />
-                    </Toolbar>
-                    <Box sx={{ height: '80vh' }} >
+
+                        <MenuList
+                            sx={{
+                                width: '100%',
+                                // bgcolor: 'background.paper',
+                                bgcolor: '#F7F7F7',
+                                position: 'relative',
+                                overflow: 'auto',
+                                height: '30vh',
+                                '& ul': { padding: 0 },
+                            }}
+                            component="nav"
+                            aria-labelledby="nested-list-subheader"
+                            subheader={
+                                <ListSubheader component="div"
+                                    sx={{
+                                        bgcolor: '#F7F7F7',
+                                    }}
+                                >
+                                    CHAT
+                                </ListSubheader>
+                            }
+
+                        >
+                            {
+                                store.chatSessions.map((session, ix) => (
+                                    <SessionItem selected={store.currentSession.id === session.id}
+                                        session={session}
+                                        switchMe={() => store.switchCurrentSession(session)}
+                                        deleteMe={() => store.deleteChatSession(session)}
+                                        copyMe={() => {
+                                            const newSession = createSession(session.name + ' Copyed')
+                                            newSession.messages = session.messages
+                                            store.createChatSession(newSession, ix)
+                                        }}
+                                        updateMe={(updated) => store.updateChatSession(updated)}
+                                    />
+                                ))
+                            }
+                        </MenuList>
+
+                        <MenuList
+                            sx={{
+                                width: '100%',
+                                // bgcolor: 'background.paper',
+                                bgcolor: '#F7F7F7',
+                                position: 'relative',
+                                overflow: 'auto',
+                                height: '30vh',
+                                '& ul': { padding: 0 },
+                            }}
+                            component="nav"
+                            aria-labelledby="nested-list-subheader"
+                            subheader={
+                                <ListSubheader component="div"
+                                    sx={{
+                                        bgcolor: '#F7F7F7',
+                                    }}
+                                >
+                                    Chat
+                                </ListSubheader>
+                            }
+
+                        >
+                            {
+                                store.chatSessions.map((session, ix) => (
+                                    <SessionItem selected={store.currentSession.id === session.id}
+                                        session={session}
+                                        switchMe={() => store.switchCurrentSession(session)}
+                                        deleteMe={() => store.deleteChatSession(session)}
+                                        copyMe={() => {
+                                            const newSession = createSession(session.name + ' Copyed')
+                                            newSession.messages = session.messages
+                                            store.createChatSession(newSession, ix)
+                                        }}
+                                        updateMe={(updated) => store.updateChatSession(updated)}
+                                    />
+                                ))
+                            }
+                        </MenuList>
+
+                        <Divider />
+
+                        <MenuItem onClick={() => store.createEmptyChatSession()} >
+                            <ListItemIcon>
+                                <IconButton><AddCircleOutlineIcon fontSize="small" /></IconButton>
+                            </ListItemIcon>
+                            <ListItemText>
+                                New Session
+                            </ListItemText>
+                            <Typography variant="body2" color="text.secondary">
+                                ⌘N
+                            </Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => {
+                            setOpenSettingWindow(true)
+                        }}
+                        >
+                            <ListItemIcon>
+                                <IconButton><AddCircleOutlineIcon fontSize="small" /></IconButton>
+                            </ListItemIcon>
+                            <ListItemText>
+                                Settings
+                            </ListItemText>
+                            <Typography variant="body2" color="text.secondary">
+                                ⌘N
+                            </Typography>
+                        </MenuItem>
+
+                    </Stack>
+
+                </Grid>
+                <Grid item xs={9}
+                    sx={{
+                        height: '100%',
+                    }}
+                >
+                    <Stack sx={{
+                        height: '100%',
+                        // bgcolor: '#F7F7F7',
+                        padding: '20px 0',
+                    }} spacing={2}>
+                        <Toolbar variant="dense">
+                            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                                <ChatBubbleOutlineOutlinedIcon />
+                            </IconButton>
+                            <Typography variant="h6" color="inherit" component="div" noWrap>
+                                {store.currentSession.name}
+                            </Typography>
+                        </Toolbar>
+                        <Divider />
                         <List
                             ref={listRef}
                             sx={{
                                 width: '100%',
-                                height: '80vh',
+                                height: '80%',
                                 bgcolor: 'background.paper',
                                 overflow: 'auto',
                                 '& ul': { padding: 0 },
@@ -202,28 +222,28 @@ function App() {
                                 ))
                             }
                         </List>
-                    </Box>
-                    <Box>
-                        <MessageInput onSubmit={async (newMsg: Message) => {
-                            store.currentSession.messages.push(newMsg)
-                            store.updateChatSession({ ...store.currentSession })
-                            const msg = await client.replay(store.settings.openaiKey, store.currentSession.messages)
-                            store.currentSession.messages.push(msg)
-                            store.updateChatSession({ ...store.currentSession })
-                        }} />
-                    </Box>
-                </Box>
-            </Grid>
+                        <Box>
+                            <MessageInput onSubmit={async (newMsg: Message) => {
+                                store.currentSession.messages.push(newMsg)
+                                store.updateChatSession({ ...store.currentSession })
+                                const msg = await client.replay(store.settings.openaiKey, store.currentSession.messages)
+                                store.currentSession.messages.push(msg)
+                                store.updateChatSession({ ...store.currentSession })
+                            }} />
+                        </Box>
+                    </Stack>
+                </Grid>
 
-            <SettingWindow open={openSettingWindow}
-                settings={store.settings}
-                save={(settings) => {
-                    store.setSettings(settings)
-                    setOpenSettingWindow(false)
-                }}
-                close={() => setOpenSettingWindow(false)}
-            />
-        </Grid>
+                <SettingWindow open={openSettingWindow}
+                    settings={store.settings}
+                    save={(settings) => {
+                        store.setSettings(settings)
+                        setOpenSettingWindow(false)
+                    }}
+                    close={() => setOpenSettingWindow(false)}
+                />
+            </Grid>
+        </Box>
     );
 }
 
@@ -243,6 +263,7 @@ function MessageInput(props: {
         >
             <Stack direction="row" spacing={1} alignItems="center">
                 <TextField
+                    multiline
                     label="Message"
                     value={messageText}
                     onChange={(event) => setMessageText(event.target.value)}
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,5 +26,5 @@
  * ```
  */
 
-// import './index.css';
+import './index.css';
 import './app';
