e750b5fe413256c795429153e7a421cdea55ad55
Bin-Huang <tohuangbin@gmail.com>
2023-03-14 22:02:50 +0800

    release: v0.1.1

--- make.sh
+++ make.sh
@@ -13,20 +13,20 @@ npx electron-forge make -p darwin -a x64
 
 mkdir -p out/release
 
-# cp out/make/deb/arm64/chatbox_0.1.0_arm64.deb out/release/chatbox_0.1.0_arm64.deb
-cp out/make/deb/x64/chatbox_0.1.0_amd64.deb out/release/chatbox_0.1.0_amd64.deb
+# cp out/make/deb/arm64/chatbox_0.1.1_arm64.deb out/release/chatbox_0.1.1_arm64.deb
+cp out/make/deb/x64/chatbox_0.1.1_amd64.deb out/release/chatbox_0.1.1_amd64.deb
 
-# cp 'out/make/squirrel.windows/arm64/chatbox-0.1.0 Setup.exe' out/release/chatbox_0.1.0_arm64_setup.exe
-cp 'out/make/squirrel.windows/x64/chatbox-0.1.0 Setup.exe' out/release/chatbox_0.1.0_x64_setup.exe
+# cp 'out/make/squirrel.windows/arm64/chatbox-0.1.1 Setup.exe' out/release/chatbox_0.1.1_arm64_setup.exe
+cp 'out/make/squirrel.windows/x64/chatbox-0.1.1 Setup.exe' out/release/chatbox_0.1.1_x64_setup.exe
 
-# cp out/make/zip/linux/arm64/chatbox-linux-arm64-0.1.0.zip out/release/chatbox-linux-arm64-0.1.0.zip
-cp out/make/zip/linux/x64/chatbox-linux-x64-0.1.0.zip out/release/chatbox-linux-x64-0.1.0.zip
+# cp out/make/zip/linux/arm64/chatbox-linux-arm64-0.1.1.zip out/release/chatbox-linux-arm64-0.1.1.zip
+cp out/make/zip/linux/x64/chatbox-linux-x64-0.1.1.zip out/release/chatbox-linux-x64-0.1.1.zip
 
-# cp out/make/zip/win32/arm64/chatbox-win32-arm64-0.1.0.zip out/release/chatbox-win32-arm64-0.1.0.zip
-cp out/make/zip/win32/x64/chatbox-win32-x64-0.1.0.zip out/release/chatbox-win32-x64-0.1.0.zip
+# cp out/make/zip/win32/arm64/chatbox-win32-arm64-0.1.1.zip out/release/chatbox-win32-arm64-0.1.1.zip
+cp out/make/zip/win32/x64/chatbox-win32-x64-0.1.1.zip out/release/chatbox-win32-x64-0.1.1.zip
 
-# cp out/make/chatbox-0.1.0-arm64.dmg out/release/chatbox-0.1.0-arm64.dmg
-cp out/make/chatbox-0.1.0-x64.dmg out/release/chatbox-0.1.0.dmg
+# cp out/make/chatbox-0.1.1-arm64.dmg out/release/chatbox-0.1.1-arm64.dmg
+cp out/make/chatbox-0.1.1-x64.dmg out/release/chatbox-0.1.1.dmg
 
-# cp out/make/zip/darwin/arm64/chatbox-darwin-arm64-0.1.0.zip out/release/chatbox-darwin-arm64-0.1.0.zip
-cp out/make/zip/darwin/x64/chatbox-darwin-x64-0.1.0.zip out/release/chatbox-darwin-0.1.0.zip
+# cp out/make/zip/darwin/arm64/chatbox-darwin-arm64-0.1.1.zip out/release/chatbox-darwin-arm64-0.1.1.zip
+cp out/make/zip/darwin/x64/chatbox-darwin-x64-0.1.1.zip out/release/chatbox-darwin-0.1.1.zip
