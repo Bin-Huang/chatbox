7fb2a9f82ccfbe8717852729f1992fcf8a51d073
Joe Goldin <joe@joegold.in>
2023-03-15 18:37:35 +0000

    use tiktoken library to estimate tokens

--- src/devtools/utils.ts
+++ src/devtools/utils.ts
@@ -24,35 +24,10 @@ export function countWord(data: string): number {
     return count;
 };
 
-export type EstimationMethod = "average" | "words" | "chars" | "max" | "min";
-export function estimateTokens(text: string, method: EstimationMethod = "max"): number | string {
-    // method can be "average", "words", "chars", "max", "min", defaults to "max"
-    const wordCount: number = text.split(" ").length;
-    const charCount: number = text.length;
-    const tokensCountWordEst: number = wordCount / 0.75;
-    const tokensCountCharEst: number = charCount / 4.0;
-    let output: number = 0;
-
-    switch (method) {
-        case "average":
-            output = (tokensCountWordEst + tokensCountCharEst) / 2;
-            break;
-        case "words":
-            output = tokensCountWordEst;
-            break;
-        case "chars":
-            output = tokensCountCharEst;
-            break;
-        case "max":
-            output = Math.max(tokensCountWordEst, tokensCountCharEst);
-            break;
-        case "min":
-            output = Math.min(tokensCountWordEst, tokensCountCharEst);
-            break;
-        default:
-            // return invalid method message
-            return "Invalid method. Use 'average', 'words', 'chars', 'max', or 'min'.";
-    }
-
-    return Math.ceil(output);
+import { encoding_for_model } from "@dqbd/tiktoken";
+const enc = encoding_for_model("text-davinci-003");
+export function estimateTokens(text: string): number | string {
+    const encoded = enc.encode(text);
+    return encoded.length;
 }
+
