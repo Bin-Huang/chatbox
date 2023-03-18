5020818ce9c202a680acaa8814bf6ed14216bb40
Joe Goldin <joe@joegold.in>
2023-03-18 05:42:16 +0000

    add `gpt-4-32k` and `gpt-4-32k-0314`

--- src/devtools/SettingWindow.tsx
+++ src/devtools/SettingWindow.tsx
@@ -9,7 +9,7 @@ import { Settings } from './types'
 import { getDefaultSettings } from './store'
 
 const { useEffect } = React
-const models: string[] = ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'gpt-4', 'gpt-4-0314'];
+const models: string[] = ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'gpt-4', 'gpt-4-0314', 'gpt-4-32k', 'gpt-4-32k-0314'];
 interface Props {
     open: boolean
     settings: Settings
