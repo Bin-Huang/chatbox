eaacb136d1438bf7b00c105564d16e426ee67f9f
Bin-Huang <tohuangbin@gmail.com>
2023-03-22 21:57:10 +0800

    chore: switch data path from AppConfig to AppData

--- src/api.ts
+++ src/api.ts
@@ -11,7 +11,7 @@ export const openLink = async (url: string) => {
 export const writeStore = async (key: string, value: any) => {
     const config = await readConfig()
     config[key] = value
-    await api.fs.writeTextFile('config.json', JSON.stringify(config), { dir: api.fs.Dir.AppConfig })
+    await api.fs.writeTextFile('config.json', JSON.stringify(config), { dir: api.fs.Dir.AppData })
 }
 
 export const readStore = async (key: string) => {
@@ -20,28 +20,28 @@ export const readStore = async (key: string) => {
 }
 
 async function readConfig() {
-    const dirExists = await api.fs.exists('', { dir: api.fs.Dir.AppConfig })
+    const dirExists = await api.fs.exists('', { dir: api.fs.Dir.AppData })
     if (!dirExists) {
         try {
-            await api.fs.createDir('', { dir: api.fs.Dir.AppConfig })
+            await api.fs.createDir('', { dir: api.fs.Dir.AppData })
         } catch (e) {
             console.log('ensure app config dir', e)
         }
     }
 
-    const configExists = await api.fs.exists('config.json', { dir: api.fs.Dir.AppConfig })
+    const configExists = await api.fs.exists('config.json', { dir: api.fs.Dir.AppData })
     if (!configExists) {
         try {
             // 从旧版本迁移
             const oldConfig = await api.fs.readTextFile('chatbox/config.json', { dir: api.fs.Dir.LocalData })
-            await api.fs.writeTextFile('config.json', oldConfig, { dir: api.fs.Dir.AppConfig })
+            await api.fs.writeTextFile('config.json', oldConfig, { dir: api.fs.Dir.AppData })
         } catch (e) {
             console.log(e)
-            await api.fs.writeTextFile('config.json', '{}', { dir: api.fs.Dir.AppConfig })
+            await api.fs.writeTextFile('config.json', '{}', { dir: api.fs.Dir.AppData })
         }
     }
 
-    const configJson = await api.fs.readTextFile('config.json', { dir: api.fs.Dir.AppConfig })
+    const configJson = await api.fs.readTextFile('config.json', { dir: api.fs.Dir.AppData })
     return JSON.parse(configJson)
 }
 
