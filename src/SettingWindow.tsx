e209490b8cfa93ac489f8dbabfbded5bebcaf144
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-04-30 14:13:41 +0900

    Feat: Japanese language support

--- src/SettingWindow.tsx
+++ src/SettingWindow.tsx
@@ -20,11 +20,12 @@ import LightbulbCircleIcon from '@mui/icons-material/LightbulbCircle';
 
 const { useEffect } = React
 const models: string[] = ['gpt-3.5-turbo', 'gpt-3.5-turbo-0301', 'gpt-4', 'gpt-4-0314', 'gpt-4-32k', 'gpt-4-32k-0314'];
-const languages: string[] = ['en', 'zh-Hans', 'zh-Hant'];
+const languages: string[] = ['en', 'zh-Hans', 'zh-Hant','jp'];
 const languageMap: { [key: string]: string } = {
     'en': 'English',
     'zh-Hans': '简体中文',
     'zh-Hant': '繁體中文',
+    'jp':'日本語'
 };
 interface Props {
     open: boolean
--- src/i18n.js
+++ src/i18n.js
@@ -4,6 +4,7 @@ import { initReactI18next } from 'react-i18next'
 import en from './i18n/locales/en/translation.json'
 import zhHans from './i18n/locales/zh-Hans/translation.json'
 import zhHant from './i18n/locales/zh-Hant/translation.json'
+import jp from './i18n/locales/jp/translation.json'
 
 const resources = {
   'zh-Hans': {
@@ -15,6 +16,9 @@ const resources = {
   'en': {
     translation: en,
   },
+	'jp':{
+		translation: jp
+	}
 }
 
 i18n
