9cea5a78584b2cc6a299f4e317d0a3f64cdc763d
AiDrive_Application <96203659+AiDriveOne@users.noreply.github.com>
2023-05-10 22:24:54 +0200

    dazugpt_prompt.py
    # File: gpt_prompt.py
    
    prompt = """
    Prompts for Multi-Task Management:
    
    1. Text and voice classification and generation:
       - Prompt: "Perform text and voice classification tasks and generate responses based on the given input."
       - Example usage: "Please classify the given text and voice inputs and generate appropriate responses."
    
    2. Image and video classification and generation:
       - Prompt: "Classify images and videos and generate relevant information or responses based on the provided content."
       - Example usage: "Please analyze the given images and videos, classify their content, and provide detailed information or generate responses accordingly."
    
    3. Website creation and building:
       - Prompt: "Create and build websites based on the provided specifications, including design, layout, and functionality."
       - Example usage: "Please develop a website based on the given requirements, including design, layout, and interactive features."
    
    4. Code generation and analysis:
       - Prompt: "Generate code snippets or analyze existing code for specific programming tasks, providing optimized solutions or identifying errors."
       - Example usage: "Please generate a code snippet for sorting an array in Python or analyze the provided code for any syntax errors or performance improvements."
    
    5. Error fixing and automatic code generation:
       - Prompt: "Automatically fix coding errors and generate clean and optimized code based on the given requirements or existing code snippets."
       - Example usage: "Please identify and fix any errors in the provided code and generate an optimized version of the code for performance improvement."
    
    6. App deployment to store with developer account credentials:
       - Prompt: "Deploy an application to an app store using the provided developer account username and password."
       - Example usage: "Please use my developer account credentials to deploy the application to the App Store or Play Store."
    
    7. App marketing on social media with input textbox links, username, and password:
       - Prompt: "Automatically market the app by sharing the app's download link on multiple social media platforms using the provided username and password."
       - Example usage: "Please automatically share the app's download link on Facebook, Twitter, and Instagram using my social media account credentials."
    
    8. Test the application:
       - Prompt: "Test an application and provide feedback on its performance, functionality, and user experience."
       - Example usage: "Please test the application for any bugs, errors, or usability issues and provide detailed feedback."
    
    9. Deploy the application:
       - Prompt: "Deploy an application to a server or hosting platform based on the provided specifications and requirements."
       - Example usage: "Please deploy the application to the specified server or hosting platform with the given specifications and requirements."
    
    10. Folders and Files Attachment:
        - Prompt: "Manage folders and files, perform operations such as creating, deleting, moving, and attaching files to emails or documents."
        - Example usage: "Please create a new folder, move the files into it, and attach the required files to the email."
    
    """
    
    # Now you can use the 'prompt' variable in your code

---
+++ dazugpt_prompt.py
@@ -0,0 +1,48 @@
+# File: gpt_prompt.py
+
+prompt = """
+Prompts for Multi-Task Management:
+
+1. Text and voice classification and generation:
+   - Prompt: "Perform text and voice classification tasks and generate responses based on the given input."
+   - Example usage: "Please classify the given text and voice inputs and generate appropriate responses."
+
+2. Image and video classification and generation:
+   - Prompt: "Classify images and videos and generate relevant information or responses based on the provided content."
+   - Example usage: "Please analyze the given images and videos, classify their content, and provide detailed information or generate responses accordingly."
+
+3. Website creation and building:
+   - Prompt: "Create and build websites based on the provided specifications, including design, layout, and functionality."
+   - Example usage: "Please develop a website based on the given requirements, including design, layout, and interactive features."
+
+4. Code generation and analysis:
+   - Prompt: "Generate code snippets or analyze existing code for specific programming tasks, providing optimized solutions or identifying errors."
+   - Example usage: "Please generate a code snippet for sorting an array in Python or analyze the provided code for any syntax errors or performance improvements."
+
+5. Error fixing and automatic code generation:
+   - Prompt: "Automatically fix coding errors and generate clean and optimized code based on the given requirements or existing code snippets."
+   - Example usage: "Please identify and fix any errors in the provided code and generate an optimized version of the code for performance improvement."
+
+6. App deployment to store with developer account credentials:
+   - Prompt: "Deploy an application to an app store using the provided developer account username and password."
+   - Example usage: "Please use my developer account credentials to deploy the application to the App Store or Play Store."
+
+7. App marketing on social media with input textbox links, username, and password:
+   - Prompt: "Automatically market the app by sharing the app's download link on multiple social media platforms using the provided username and password."
+   - Example usage: "Please automatically share the app's download link on Facebook, Twitter, and Instagram using my social media account credentials."
+
+8. Test the application:
+   - Prompt: "Test an application and provide feedback on its performance, functionality, and user experience."
+   - Example usage: "Please test the application for any bugs, errors, or usability issues and provide detailed feedback."
+
+9. Deploy the application:
+   - Prompt: "Deploy an application to a server or hosting platform based on the provided specifications and requirements."
+   - Example usage: "Please deploy the application to the specified server or hosting platform with the given specifications and requirements."
+
+10. Folders and Files Attachment:
+    - Prompt: "Manage folders and files, perform operations such as creating, deleting, moving, and attaching files to emails or documents."
+    - Example usage: "Please create a new folder, move the files into it, and attach the required files to the email."
+
+"""
+
+# Now you can use the 'prompt' variable in your code
